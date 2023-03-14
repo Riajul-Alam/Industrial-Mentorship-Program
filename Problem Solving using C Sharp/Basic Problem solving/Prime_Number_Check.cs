c2135557b60bcdc56b5ff5ff5d631dc4511560f6
riajul-alam <riajulalam5@gmail.com>
2023-03-14 14:35:49 +0600

    added new file

---
+++ Problem Solving using C Sharp/Basic Problem solving/Prime_Number_Check.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Program
+{
+    class Prime
+    {
+        public static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            bool flag=false;
+            // i<=Math.Sqrt(n)
+            for(int i = 2; i*i<=n; i++)
+            {
+                if (n%i==0)
+                {
+                    Console.WriteLine("Not Prime number");
+                    flag = true;break;
+                }
+            }
+            if (!flag)
+            {
+                Console.WriteLine("Prime number");
+            }
+        }
+    }
+}
