f5c39e6f497564e999b5771d0ac0624da55661b4
riajul-alam <riajulalam5@gmail.com>
2023-03-30 02:40:30 +0600

    Interface example added..

---
+++ OOP Practice/InterfaceExample/InterfaceExample/Program.cs
@@ -0,0 +1,18 @@
+﻿using InterfaceExample;
+
+
+
+
+
+Student student = new Student();
+student.RegNo=125454;
+
+string info=student.GetBasicInformation();
+
+Department department = new Department();
+department.Code="CSE-4544";
+string deptinfo=department.GetBasicInformation();
+
+Course course = new Course();
+course.Code="565656";
+string courseInfo=course.GetBasicInformation();
---
+++ OOP Practice/InterfaceExample/InterfaceExample/Student.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterfaceExample
+{
+    public class Student:IInformation
+    {
+        public int RegNo { get; set; }
+        public int Name { get; set; }
+        public int Roll { get; set; }
+        public int Email { get; set; }
+
+        public string GetBasicInformation()
+        {
+            return "RegNo : "+RegNo+" Name : "+Name+" Roll : "+Roll;
+        }
+    }
+}
