e68243c4fb158d1194e5547ca11ed1a6376ebc62
riajul-alam <riajulalam5@gmail.com>
2023-03-31 05:31:27 +0600

    "IEnumerable Interface practice problem2 added.. "

---
+++ OOP Practice/IEnumerable_Interface/IEnumerable_Interface2/IEnumerable_Interface2/Organization.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IEnumerable_Interface2
+{
+    public class Organization : IEnumerable
+    {
+        List<Employee> Emps = new List<Employee>();
+        public void Add(Employee emp)
+        {
+            Emps.Add(emp);
+        }
+        public int Count { get { return Emps.Count; } }
+
+        public Employee this[int index]
+        {
+            get { return Emps[index]; }
+        }
+        public IEnumerator GetEnumerator()
+        {
+            return new OrganizationEnumerator(this);
+        }
+    }
+}
