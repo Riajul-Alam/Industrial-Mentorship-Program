b40c65d0aeeccb29a262e6410be975d6e569449f
riajul-alam <riajulalam5@gmail.com>
2023-04-01 06:35:43 +0600

    ICollection interface practice problem added..

---
+++ OOP Practice/ICollection_Interface/ICollectionExample/ICollectionExample/Program.cs
@@ -0,0 +1,23 @@
+﻿using ICollectionExample;
+
+MyCollection myCollection = new MyCollection();
+
+myCollection.Add("apple");
+myCollection.Add("banana");
+myCollection.Add("cherry");
+
+Console.WriteLine(myCollection.Count);
+
+foreach (string item in myCollection)
+{
+    Console.WriteLine(item);
+}
+
+myCollection.Remove("banana");
+
+Console.WriteLine(myCollection.Count);
+
+foreach (string item in myCollection)
+{
+    Console.WriteLine(item);
+}
