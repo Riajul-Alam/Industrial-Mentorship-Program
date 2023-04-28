1f949403ae0518362658c919461211fc4479df94
riajul-alam <riajulalam5@gmail.com>
2023-04-28 15:50:12 +0600

    updated

---
+++ OOP Practice/Generic_class Assignment/GenericTask_01/GenericTask_01/Program.cs
@@ -0,0 +1,14 @@
+﻿using GenericTask_01;
+
+
+
+Pair<string, int> myPair = new Pair<string, int>("Riajul", "Alam");
+Pair<int, string> myPair1 = new Pair<int, string>(1, 56);
+
+Console.WriteLine("First: "+myPair.First+","+" Second: "+myPair.Second);
+Console.WriteLine("First: "+myPair1.First+","+" Second: "+myPair1.Second);
+
+
+
+
+
