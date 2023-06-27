03f8e961e8d5cb568316e66bf5109cf581583cf1
riajul-alam <riajulalam5@gmail.com>
2023-06-27 14:55:40 +0600

    Middleware Example

---
+++ ASP NET Core/Middleware/Middleware/Middleware/MiddlewareServices/CustomMiddleware.cs
@@ -0,0 +1,17 @@
+﻿namespace Middleware.MiddlewareServices
+{
+    public class CustomMiddleware : IMiddleware
+    {
+        public async Task InvokeAsync(HttpContext context, RequestDelegate next)
+        {
+            await context.Response.WriteAsync("Hello Dear Readers from Custom Middleware!");
+        }
+    }
+    public static class ClassWithNoImplementationMiddlewareExtensions
+    {
+        public static IApplicationBuilder UseClassWithNoImplementationMiddleware(this IApplicationBuilder builder)
+        {
+            return builder.UseMiddleware<CustomMiddleware>();
+        }
+    }
+}
