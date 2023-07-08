c295f36b1640633aae819269360aea9b78b4c359
riajul-alam <riajulalam5@gmail.com>
2023-07-09 02:28:18 +0600

    Middleware example updated

---
+++ ASP .NET Core/Middleware/4.CustomMiddlewareExtension/CustomMiddlewareExtension/CustomExtension/MyExtension.cs
@@ -0,0 +1,19 @@
+﻿namespace CustomMiddlewareExtension.CustomExtension
+{
+    public class MyExtension : IMiddleware
+    {
+        public async Task InvokeAsync(HttpContext context, RequestDelegate next)
+        {
+            await context.Response.WriteAsync("Custom Middleware started!\n\n");
+            await next(context);
+            await context.Response.WriteAsync("Custom Middleware finished!\n\n");
+        }
+    }
+    public static class CustomMiddlewareExtension
+    {
+        public static IApplicationBuilder MyExtension(this IApplicationBuilder app)
+        {
+            return app.UseMiddleware<MyExtension>();
+        }
+    }
+}
